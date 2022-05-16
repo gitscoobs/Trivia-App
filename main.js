5a5d8fbda5184d2e498d7931a549ad9126dd1d08
gitscoobs <nspinzola@gmail.com>
2022-05-16 16:49:13 -0400

    Changed background, attempts made at randomizing answers to buttons

--- main.js
+++ main.js
@@ -1,7 +1,6 @@
 const startButton = document.getElementById('start-btn')
 const questionContainerElement = document.getElementById('question-container')
 
-
 startButton.addEventListener('click' , startGame)
 
 function startGame() {
@@ -11,11 +10,11 @@ function startGame() {
   
 }
 
+//https://opentdb.com/api.php?amount=5
 // function to fetch data from the api
-fetch(`https://opentdb.com/api.php?amount=5`) 
+fetch(`https://opentdb.com/api.php?amount=5&type=multiple`) 
 .then(response => response.json())
 .then(data => addQuestions(data))
-
 
 function addQuestions(data){
     
@@ -23,32 +22,49 @@ function addQuestions(data){
     var corAnswer = data.results[i].correct_answer
     var question = data.results[i].question
 
-    const questionElement = document.createElement("div");
+    const questionElement = document.createElement('div');
     questionElement.innerHTML = question;
     document.getElementById("questionDiv").appendChild(questionElement);
 
-    const corAnswerElement = document.createElement("div");
-    corAnswerElement.innerHTML = corAnswer;
-    document.getElementById("corDiv").appendChild(corAnswerElement);
+    // const corAnswerElement = document.createElement('button');
+    // corAnswerElement.innerHTML = corAnswer;
+    //  document.getElementById("corDiv").appendChild(corAnswerElement);
 
       for(let j = 0; j < data.results[i].incorrect_answers.length;  j++){
         var incAnswer = data.results[i].incorrect_answers[j]
 
-          const incAnswerElement = document.createElement("div");
-          incAnswerElement.innerHTML = incAnswer;
-          document.getElementById("incDiv").appendChild(incAnswerElement);
+        //Attempt at shuffling answers together 
+        // var incAnswerButton = data.results[i].incorrect_answers[j]
+        // var corAnswerButton = data.results[i].correct_answer
+        // function shuffleArray(arr) {
+        //   arr.sort(() => Math.random() - 0.5);
+        // }
+        // let arr = [incAnswerButton];
+        // shuffleArray(arr);
+        // console.log(arr)
+
+        // const incAnswerElement = document.createElement('button');
+        // incAnswerElement.innerHTML = incAnswer;
+        // document.getElementById("incDiv").appendChild(incAnswerElement);
+
+        // const merge = (first, second) => {
+        //   for(let i=0; i<second.length; i++) {
+        //     first.push(second[i]);
+        //   }
+        //   return first;
+        // }
+        
+        // let merged = merge([corAnswer], [incAnswer]);
+        // console.log(merged);
           
+
+        // let merged = [...corAnswer, ...incAnswer];
+        // console.log(merged);
+
+        
+
+      
+
         }
-  }
+    }
 }
-
-
-
-
-
-
-
-
-
-
- 
